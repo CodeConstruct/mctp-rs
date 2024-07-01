8d864f992f0c91d611ecd20cbaf067a0c19339e2
Matt Johnston <matt@codeconstruct.com.au>
2024-07-01 16:31:27 +0800

    pldm-fw: Allow building pldm-fw with no_std
    The Update Agent still requires std. Firmware Device implementation will
    work with no_std.
    
    Signed-off-by: Matt Johnston <matt@codeconstruct.com.au>

---
+++ pldm-fw/src/fd.rs
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+ * PLDM firmware update utility.
+ *
+ * Copyright (c) 2023 Code Construct
+ */
+
+//! PLDM Firmware Update Agent
+//!
+//! Update Agent requires `std` feature.
+// use log::{debug, error};
+
+// use nom::{
+//     combinator::{all_consuming, complete, map},
+//     multi::length_value,
+//     number::complete::le_u32,
+//     sequence::tuple,
+//     IResult,
+// };
+
+// use pldm::PldmError;
+
+
+
