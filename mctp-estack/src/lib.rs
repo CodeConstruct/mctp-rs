989b8fb82181f98bd9e2a6d86a993db0118df556
Matt Johnston <matt@codeconstruct.com.au>
2024-07-03 12:52:12 +0800

    mctp-estack: Add i2c transport
    Performs encapsulation/decapsulation, and can fragment messages for
    transmission.
    
    Signed-off-by: Matt Johnston <matt@codeconstruct.com.au>

--- mctp-estack/src/lib.rs
+++ mctp-estack/src/lib.rs
@@ -26,8 +26,10 @@ use heapless::LinearMap;
 use mctp::{Eid, MsgType, TagValue, Tag, Error, Result};
 
 mod fragment;
+mod i2c;
 mod reassemble;
 
+pub use crate::i2c::{MctpI2cEncap, MCTP_I2C_COMMAND_CODE, MctpI2cHandler};
 pub use fragment::{Fragmenter, SendOutput};
 use reassemble::Reassembler;
 
