b2207708b2921c0286a30dd388076027ed12f14e
Matt Johnston <matt@codeconstruct.com.au>
2024-07-03 12:52:12 +0800

    mctp-estack: Add MCTP embedded stack
    This is a platform independent MCTP stack implementation to run on
    embedded devices with no heap allocation.
    
    This initial implementation includes the common MCTP stack and MCTP over
    I2C transport implementation.
    
    Signed-off-by: Matt Johnston <matt@codeconstruct.com.au>

---
+++ mctp-estack/src/i2c.rs
@@ -0,0 +1,271 @@
+#[allow(unused)]
+use log::{debug, error, info, trace, warn};
+
+use crate::{
+    AppCookie, Fragmenter, MctpMessage, ReceiveHandle, SendOutput, Stack,
+};
+use mctp::{Eid, Error, MsgType, Result, Tag};
+
+use heapless::Vec;
+
+pub const MCTP_I2C_COMMAND_CODE: u8 = 0x0f;
+
+const MCTP_I2C_HEADER: usize = 4;
+// bytecount is limited to u8, includes MCTP payload + 1 byte i2c source
+const MCTP_I2C_MAXMTU: usize = u8::MAX as usize - 1;
+
+/// Size of fixed transmit buffer. This is the maximum size of a MCTP message.
+///
+/// *TODO:* This will be replaced with a dynamically sized `send_message` buffer
+/// passed in.
+///
+/// Requires finding a replacement for `Vec` in `fill_msg` for `send_enqueue()`.
+/// Maybe heapless::VecView once it's in a release.
+pub const SENDBUF: usize = 1024;
+
+type MctpI2cHeader =
+    libmctp::smbus_proto::MCTPSMBusHeader<[u8; MCTP_I2C_HEADER]>;
+
+/// Simple packet processing to add/remove the 4 byte MCTP-I2C header.
+#[derive(Debug)]
+pub struct MctpI2cEncap {
+    own_addr: u8,
+}
+
+impl MctpI2cEncap {
+    pub fn new(own_addr: u8) -> Self {
+        Self { own_addr }
+    }
+
+    /// Handles a MCTP fragment with the PEC already validated
+    ///
+    /// `packet` should omit the PEC byte.
+    /// Returns the MCTP message and the i2c source address.
+    pub fn receive_done_pec<'f>(
+        &mut self,
+        packet: &[u8],
+        mctp: &'f mut Stack,
+    ) -> Result<Option<(MctpMessage<'f>, u8, ReceiveHandle)>> {
+        if packet.len() < MCTP_I2C_HEADER {
+            return Err(Error::InvalidInput);
+        }
+
+        let (i2c, packet) = packet.split_at(MCTP_I2C_HEADER);
+        // OK unwrap, size matches
+        let header = MctpI2cHeader::new_from_buf(i2c.try_into().unwrap());
+        // +1 for i2c source address field
+        if header.byte_count() as usize != packet.len() + 1 {
+            return Err(Error::InvalidInput);
+        }
+
+        if header.command_code() != MCTP_I2C_COMMAND_CODE {
+            return Err(Error::InvalidInput);
+        }
+
+        // Pass to MCTP stack
+        let m = mctp.receive(packet)?;
+
+        // Return a (message, i2c_source) tuple on completion
+        Ok(m.map(|(msg, handle)| (msg, header.source_slave_addr(), handle)))
+    }
+
+    /// `out` must be sized to hold 8+mctp_mtu, to allow for MCTP and I2C headers
+    ///
+    /// TODO: optionally add PEC.
+    pub fn send<'f>(
+        &self,
+        i2c_dest: u8,
+        payload: &[u8],
+        out: &'f mut [u8],
+        fragmenter: &mut Fragmenter,
+    ) -> SendOutput<'f> {
+        if out.len() < MCTP_I2C_HEADER {
+            return SendOutput::failure(Error::InvalidInput, fragmenter);
+        }
+
+        let (i2chead, packet) = out.split_at_mut(MCTP_I2C_HEADER);
+
+        // Get a packet from the fragmenter
+        let r = fragmenter.fragment(payload, packet);
+        let packet = match r {
+            SendOutput::Packet(packet) => packet,
+            // Just return on Complete or Error
+            | SendOutput::Complete { .. }
+            | SendOutput::Error { .. }
+            => return r.unborrowed().unwrap(),
+        };
+
+        // Write the i2c header and return the whole packet
+        let mut header = MctpI2cHeader::new();
+        header.set_dest_slave_addr(i2c_dest);
+        header.set_source_slave_addr(self.own_addr);
+        header.set_command_code(MCTP_I2C_COMMAND_CODE);
+        debug_assert!(packet.len() <= MCTP_I2C_MAXMTU);
+        header.set_byte_count((packet.len() + 1) as u8);
+
+        i2chead.copy_from_slice(&header.0);
+
+        let out_len = MCTP_I2C_HEADER + packet.len();
+        let out = &mut out[..out_len];
+        SendOutput::Packet(out)
+    }
+}
+
+/// A handler for I2C MCTP
+///
+/// One instance should exist for each I2C bus with a MCTP transport.
+pub struct MctpI2cHandler {
+    encap: MctpI2cEncap,
+
+    // TODO: replace with a &[u8] or similar so that we can avoid
+    // the const SENDBUF.
+    send_message: &'static mut Vec<u8, SENDBUF>,
+    send_state: HandlerSendState,
+}
+
+impl core::fmt::Debug for MctpI2cHandler {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("MctpI2cHandler")
+            .field("send_state", &self.send_state)
+            .finish_non_exhaustive()
+    }
+}
+
+impl MctpI2cHandler {
+    /// Constructs a new `MctpI2cHandler`.
+    ///
+    /// Note that the `heapless::Vec` for `fill_msg` is the version
+    /// specified by `mctp-estack` crate. The re-export
+    /// [`mctp_estack::Vec`](crate::Vec) can be used for API compatibility.
+    pub fn new(
+        own_addr: u8,
+        send_message: &'static mut Vec<u8, SENDBUF>,
+    ) -> Self {
+        Self {
+            encap: MctpI2cEncap::new(own_addr),
+            send_message,
+            send_state: HandlerSendState::Idle,
+        }
+    }
+
+    /// Handles receiving an I2C target write
+    ///
+    /// Expects to be passed a `packet` starting from the MCTP I2C header
+    /// (first byte is destination address).
+    /// `buf` should have the PEC byte removed, already checked by callers.
+    ///
+    /// *TODO:* provide separate software PEC check function?
+    pub fn receive<'f>(
+        &mut self,
+        packet: &[u8],
+        mctp: &'f mut Stack,
+    ) -> Result<Option<(MctpMessage<'f>, u8, ReceiveHandle)>> {
+        self.encap.receive_done_pec(packet, mctp)
+    }
+
+    /// Indicates whether data is pending to send
+    pub fn send_ready(&self) -> bool {
+        matches!(self.send_state, HandlerSendState::Sending { .. })
+    }
+
+    /// Fill a buffer with a packet to send over the i2c bus.
+    ///
+    /// The `send_complete` closure is called when an entire message completes sending.
+    /// It is called with `Some(Tag)` on success (with the tag that was sent)
+    /// or `None` on failure. The cookie is the one provided to [`send_enqueue`](Self::send_enqueue).
+    pub fn send_fill<'f>(&mut self, buf: &'f mut [u8]) -> SendOutput<'f> {
+        let HandlerSendState::Sending {
+            fragmenter,
+            i2c_dest,
+        } = &mut self.send_state
+        else {
+            debug_assert!(false, "called when not !send_ready()");
+            return SendOutput::bare_failure(Error::Other);
+        };
+
+        let r = self
+            .encap
+            .send(*i2c_dest, self.send_message, buf, fragmenter);
+        match r {
+            SendOutput::Complete { .. } | SendOutput::Error { .. } => {
+                self.send_message.clear();
+                self.send_state = HandlerSendState::Idle;
+            }
+            SendOutput::Packet(_) => (),
+        };
+        r
+    }
+
+    pub fn cancel_send(&mut self) -> Option<AppCookie> {
+        let mut cookie = None;
+        if let HandlerSendState::Sending { fragmenter, .. } =
+            &mut self.send_state
+        {
+            cookie = fragmenter.cookie();
+        }
+        self.send_message.clear();
+        self.send_state = HandlerSendState::Idle;
+        cookie
+    }
+
+    /// Indicates whether the send queue is idle, ready for an application to enqueue a new message
+    pub fn send_idle(&self) -> bool {
+        matches!(self.send_state, HandlerSendState::Idle)
+    }
+
+    /// Provides a MCTP message to send.
+    ///
+    /// The provided closure will fill out the message buffer, returning
+    /// `Some(())` on success. If the closure fails it returns `None`, and
+    /// `send_enqueue()` will return `mctp::Error::InvalidInput`.
+    ///
+    /// `send_enqueue()` must only be called when `send_idle()` is true.
+    /// TODO `fill_msg` will take something that isn't a Vec.
+    ///
+    /// Note that the `heapless::Vec` for `fill_msg` is the version
+    /// specified by `mctp-estack` crate. The re-export
+    /// [`mctp_estack::Vec`](crate::Vec) can be used for API compatibility.
+    pub fn send_enqueue<F>(
+        &mut self,
+        eid: Eid,
+        typ: MsgType,
+        tag: Option<Tag>,
+        ic: bool,
+        i2c_dest: u8,
+        cookie: Option<AppCookie>,
+        mctp: &mut Stack,
+        fill_msg: F,
+    ) -> Result<()>
+    where
+        F: FnOnce(&mut Vec<u8, SENDBUF>) -> Option<()>,
+    {
+        if !self.send_idle() {
+            return Err(Error::Other);
+        }
+        // debug_assert!(self.send_message.is_empty());
+        if !self.send_message.is_empty() {
+            trace!("sendmsg not empty");
+        }
+
+        // Retrieve data from the app. This may be a simple copy, or
+        // going through a kernel to fetch the message.
+        fill_msg(self.send_message).ok_or(Error::InvalidInput)?;
+
+        let fragmenter =
+            mctp.start_send(eid, typ, tag, ic, Some(MCTP_I2C_MAXMTU), cookie)?;
+        self.send_state = HandlerSendState::Sending {
+            fragmenter,
+            i2c_dest,
+        };
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+enum HandlerSendState {
+    Idle,
+    Sending {
+        fragmenter: Fragmenter,
+        i2c_dest: u8,
+    },
+}
---
+++ mctp-estack/src/reassemble.rs
@@ -0,0 +1,274 @@
+#[allow(unused)]
+use log::{debug, error, info, trace, warn};
+
+use mctp::MCTP_HEADER_VERSION_1;
+
+use crate::*;
+
+#[derive(Debug)]
+enum State {
+    New,
+    Active { typ: MsgType, ic: bool, next_seq: u8 },
+    Done { typ: MsgType, ic: bool },
+    /// An error must be returned whenver Bad state is set,
+    /// and the caller will dispose of the Reassembler.
+    Bad,
+}
+
+#[derive(Debug)]
+pub(crate) struct Reassembler {
+    // Expected local EID
+    pub own_eid: Eid,
+    // Destination EID of currently reassembled packets.
+    // Will usually match own_eid, but can also be MCTP_ADDR_NULL.
+    pub dest_eid: Eid,
+
+    pub peer: Eid,
+    pub tag: Tag,
+    pub cookie: Option<AppCookie>,
+    state: State,
+    // Set true when the ReceiveHandle to this reassembler exists.
+    handle_taken: bool,
+    // Time of SOM received for Active state, or time of EOM for Done state.
+    pub stamp: EventStamp,
+}
+
+impl Reassembler {
+    pub fn new(own_eid: Eid, packet: &[u8], stamp: EventStamp) -> Result<Self> {
+        let header = Self::header(packet)?;
+
+        let dest_eid = Eid(header.dest_endpoint_id());
+        // Allow NULL EID for physical addressing
+        if !(dest_eid == own_eid || dest_eid == mctp::MCTP_ADDR_NULL) {
+            return Err(Error::InvalidInput);
+        }
+
+        let peer = Eid(header.source_endpoint_id());
+        if peer == mctp::MCTP_ADDR_ANY {
+            return Err(Error::InvalidInput);
+        }
+
+        let tag = if header.to() == 1 {
+            Tag::Owned(TagValue(header.msg_tag()))
+        } else {
+            Tag::Unowned(TagValue(header.msg_tag()))
+        };
+
+        if header.som() != 1 {
+            // A reassembler always starts with a SOM
+            return Err(Error::InvalidInput)
+        }
+
+        Ok(Self {
+            own_eid,
+            dest_eid,
+            peer,
+            tag,
+
+            state: State::New,
+            cookie: None,
+            handle_taken: false,
+            stamp,
+        })
+    }
+
+    /// Receive a packet, returning a message when complete.
+    ///
+    /// Returns `Ok(Some(_))` when a full message is reassembled.
+    /// Returns `Ok(None)` on success when the message is incomplete.
+    pub fn receive<'f, const N: usize>(
+        &mut self,
+        packet: &[u8],
+        message: &'f mut Vec<u8, N>,
+        stamp: EventStamp,
+    ) -> Result<Option<MctpMessage<'f>>> {
+        if !self.matches_packet(packet) {
+            // Callers should have already checked matches_packet().
+            // This should be a cheap check (or optimised away?) but
+            // could become a debug assertion if needed.
+            return Err(Error::InvalidInput);
+        }
+
+        let header = Self::header(packet)?;
+        let som = header.som() == 1;
+        let eom = header.eom() == 1;
+        let min = HEADER_LEN + som as usize;
+        if packet.len() < min {
+            // TODO counters
+            debug!("Short packet");
+            return Err(Error::InvalidInput);
+        }
+        let payload = &packet[min..];
+
+        if som {
+            let (typ, ic) = mctp::decode_type_ic(packet[HEADER_LEN]);
+            let next_seq = header.pkt_seq();
+
+            self.state = State::Active { next_seq, typ, ic };
+
+            // New SOM packet restarts reassembly
+            if !message.is_empty() {
+                // TODO counters
+                debug!("Duplicate SOM");
+            }
+            message.clear();
+            self.stamp = stamp;
+        }
+
+        let State::Active {
+            typ,
+            ic,
+            ref mut next_seq,
+        } = self.state
+        else {
+            // TODO counters
+            debug!("Unexpected packet state");
+            return Err(Error::InvalidInput);
+        };
+
+        if header.pkt_seq() == *next_seq {
+            *next_seq = (*next_seq + 1) & mctp::MCTP_SEQ_MASK;
+        } else {
+            // Bad sequence halts reassembly
+            // TODO counters
+            debug!("Bad seq got {} expect {}", header.pkt_seq(), next_seq);
+            self.state = State::Bad;
+            message.clear();
+            return Err(Error::InvalidInput);
+        }
+
+        message.extend_from_slice(payload).map_err(|_| {
+            self.state = State::Bad;
+            trace!("nospace message too long");
+            Error::NoSpace
+        })?;
+
+        if eom {
+            self.state = State::Done { typ, ic };
+            self.stamp = stamp;
+            trace!("message reassembly complete, len {}", message.len());
+            return Ok(Some(self.message(message)?));
+        }
+
+        Ok(None)
+    }
+
+    /// Must be called in Done state
+    pub fn message<'f, const N: usize>(
+        &self,
+        message: &'f mut Vec<u8, N>,
+    ) -> Result<MctpMessage<'f>> {
+        let State::Done { typ, ic } = self.state else {
+            return Err(Error::BadArgument);
+        };
+
+        Ok(MctpMessage {
+            source: self.peer,
+            dest: self.own_eid,
+            tag: self.tag,
+
+            typ,
+            ic,
+            payload: message.as_slice(),
+            cookie: self.cookie,
+        })
+    }
+
+    pub fn matches_packet(&self, packet: &[u8]) -> bool {
+        if self.is_done() {
+            return false;
+        }
+
+        let Ok(header) = Self::header(packet) else {
+            return false;
+        };
+
+        self.peer == Eid(header.source_endpoint_id())
+            && self.dest_eid == Eid(header.dest_endpoint_id())
+            && self.tag.tag() == TagValue(header.msg_tag())
+            && self.tag.is_owner() == (header.to() == 1)
+    }
+
+    /// Check timeouts
+    ///
+    /// Returns `None` if timed out, `Some(remaining)` otherwise.
+    pub fn check_expired(&self, now: &EventStamp,
+        reassemble_timeout: u32,
+        done_timeout: u32) -> Option<u32> {
+
+        let timeout = match self.state {
+            State::Active { .. } => reassemble_timeout,
+            State::Done { .. } if self.handle_taken => {
+                // If a handle is outstanding the reassembler
+                // can't be cleaned up.
+                return Some(u32::MAX);
+            },
+            State::Done { .. } => done_timeout,
+            State::New | State::Bad => {
+                // Bad ones should have been cleaned up, New ones should
+                // have moved to Active prior to check_expired().
+                debug_assert!(false, "Bad or new reassembler");
+                return None
+            }
+        };
+        self.stamp.check_timeout(now, timeout)
+    }
+
+    fn header(packet: &[u8]) -> Result<Header> {
+        if packet.len() < HEADER_LEN {
+            warn!("bad len {:?}", packet);
+            return Err(Error::InvalidInput)
+        }
+
+        // OK unwrap, size is fixed
+        let hd = packet[..HEADER_LEN].try_into().unwrap();
+        let header = Header::new_from_buf(hd, 1).map_err(|e| {
+            warn!("bad header {e:?}");
+            Error::InvalidInput
+        })?;
+
+        if header.hdr_version() != MCTP_HEADER_VERSION_1 {
+            warn!("wrong version 0x{:02x}", header.hdr_version());
+            return Err(Error::InvalidInput)
+        }
+
+        Ok(header)
+    }
+
+    pub(crate) fn set_cookie(&mut self, cookie: Option<AppCookie>) {
+        if self.cookie.is_some() {
+            trace!("replacing reassember cookie")
+        }
+        self.cookie = cookie;
+    }
+
+    pub(crate) fn is_done(&self) -> bool {
+        matches!(self.state, State::Done { .. } )
+    }
+
+    pub fn handle_taken(&self) -> bool {
+        self.handle_taken
+    }
+
+    /// Track whether an assember is in use or not
+    ///
+    /// Must only be called when in `Done` state, and only one handle
+    /// may be in use at a time.
+    pub(crate) fn take_handle(&mut self, i: usize) -> ReceiveHandle {
+        debug_assert!(!self.handle_taken);
+        debug_assert!(self.is_done());
+        self.handle_taken = true;
+        ReceiveHandle(i)
+    }
+
+    pub(crate) fn return_handle(&mut self, _r: ReceiveHandle) {
+        debug_assert!(self.handle_taken);
+        self.handle_taken = false;
+    }
+}
+
+impl Drop for Reassembler {
+    fn drop(&mut self) {
+        debug_assert!(!self.handle_taken, "Outstanding handle for dropped reassembler")
+    }
+}
