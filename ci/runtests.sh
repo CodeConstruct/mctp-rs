4ec08c3c08f6917c4fe43882978b75ffdb893e51
Matt Johnston <matt@codeconstruct.com.au>
2025-02-18 15:24:37 +0800

    ci: Update runtests for defmt
    This fixes builds with/without defmt.
    
    Signed-off-by: Matt Johnston <matt@codeconstruct.com.au>

--- ci/runtests.sh
+++ ci/runtests.sh
@@ -11,8 +11,8 @@ export RUSTDOCFLAGS='-D warnings'
 export RUSTFLAGS="-D warnings"
 
 # stable, std
-cargo build --release
-cargo test
+cargo build --release --features mctp-estack/log
+cargo test --features mctp-estack/log
 
 # stable, no_std
 NOSTD_CRATES="mctp pldm pldm-fw"
@@ -23,6 +23,13 @@ for c in $NOSTD_CRATES; do
     )
 done
 
-cargo doc
+# mctp-estack combinations
+(
+cd mctp-estack
+cargo build --target thumbv7em-none-eabihf --features defmt --no-default-features
+cargo build --features log
+)
+
+cargo doc --features mctp-estack/log
 
 echo success
