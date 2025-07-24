5f3e530626aed96e2def35de3c4ce95faf014133
Jeremy Kerr <jk@codeconstruct.com.au>
2025-07-24 14:20:24 +0800

    pldm-file: Implement part negotiation in example client
    ... and merge the two block_on() invocations.
    
    Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

--- pldm-file/examples/client.rs
+++ pldm-file/examples/client.rs
@@ -1,6 +1,7 @@
 use anyhow::{Context, Result};
 use mctp::Eid;
 use mctp_linux::MctpLinuxAsyncReq;
+use pldm::control::requester::negotiate_transfer_parameters;
 use pldm_file::{
     client::{df_open, df_properties, df_read},
     proto::{DfOpenAttributes, DfProperty, FileIdentifier},
@@ -11,16 +12,22 @@ const EID: Eid = Eid(8);
 fn main() -> Result<()> {
     let mut req = MctpLinuxAsyncReq::new(EID, None)?;
 
-    let (mcm_prop, fds_prop) = smol::block_on(async {
-        (
-            df_properties(&mut req, DfProperty::MaxConcurrentMedium).await,
-            df_properties(&mut req, DfProperty::MaxFileDescriptors).await,
-        )
-    });
-
-    println!("Max Concurrent Medium: {mcm_prop:?}, Max FDs: {fds_prop:?}");
-
     smol::block_on(async {
+        let mcm_prop =
+            df_properties(&mut req, DfProperty::MaxConcurrentMedium).await;
+        let fds_prop =
+            df_properties(&mut req, DfProperty::MaxFileDescriptors).await;
+        println!("Max Concurrent Medium: {mcm_prop:?}, Max FDs: {fds_prop:?}");
+
+        let req_types = [pldm_file::PLDM_TYPE_FILE_TRANSFER];
+        let mut buf = [0u8];
+
+        let (size, neg_types) =
+            negotiate_transfer_parameters(&mut req, &req_types, &mut buf, 512)
+                .await?;
+
+        println!("Negotiated multipart size {size} for types {neg_types:?}");
+
         let id = FileIdentifier(0);
         let attrs = DfOpenAttributes::empty();
         let fd = df_open(&mut req, id, attrs)
